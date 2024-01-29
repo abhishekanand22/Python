3e881fe0ee14df27a72d75206ea58e9ede15d422
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-01-29 12:48:29 +0530

    Basic Calculator with simple functions

--- Practice Questions/calc.py
+++ Practice Questions/calc.py
@@ -1,3 +1,30 @@
-a=2
-b=3
-print("sum= ",a+b)
+# Basic Calculator 
+
+def calculator(a,b,opr):
+    if opr == "+":
+        print("Sum = ",a+b)
+    elif opr == "-":
+        print("Subtraction = ",a-b)
+    elif opr == "*":
+        print("Multiplication = ",a*b)
+    elif opr == "/":
+        print("Division = ",a/b)
+    elif opr == "%":
+        print("Percentage = ",(a/100)*b,"%")
+    else :
+        print("Invalid Operation")
+
+
+if __name__ == "__main__":
+    a=int(input("First Number : "))
+    b=int(input("Second Number : "))
+
+    opr=input("Choose Operation (+ , - , * , / , % ) : ")
+
+    if opr == "%":
+        print("In Percentage 'a' should be percent & 'b' should be total ")
+    else:
+        pass
+
+calculator(a,b,opr)
+
