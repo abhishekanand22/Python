8449d73e3396da2c0550394f1fff5dbf4e3ca54d
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-02-07 18:21:19 +0530

    File I/O

---
+++ Practice Questions/inputoutput.py
@@ -0,0 +1,17 @@
+# open a new file 
+f=open("C:\My work\Coding\Python\Practice Questions\sample.txt.txt","r")
+data = f.read()
+print(data)
+
+
+
+
+
+# writing to a file
+
+f=open("C:\My work\Coding\Python\Practice Questions\sample.txt.txt","w")
+
+f.write("Third line using")
+
+f.close()
+
