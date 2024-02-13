f3721f35f845cf83457b50fec2c8115acc322dc4
abhishekanand22 <m.abhishekanand9@gmail.com>
2024-02-13 17:38:48 +0530

    oops concepts

---
+++ Practice Questions/oops.py
@@ -0,0 +1,15 @@
+# oops
+
+class Student:
+    def __init__(self,fullname):
+        self.name=fullname
+        print("adding new student")
+    name="rohan"
+
+s1=Student("karan")
+print(s1.name)
+
+
+s2=Student("Abhishek")
+print(s2.name)
+
